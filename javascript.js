1b8af4b0467a77352267e85537b19373e4745410
Stephan Haak <info@stephan-haak.com>
2024-11-11 17:35:56 +0100

    add  function: minus/mal/durch/ istgleich/CE; optimize design "Tasten"/"Ergebnis"/"Logfile"

--- javascript.js
+++ javascript.js
@@ -1,15 +1,10 @@
 var rechenergebnis = 0;
 var wertA = false;
 var wertB = false;
-// function addition() {
-//     if{wertA=false}
-//     wertA= document.getElementById(tasteWert).value
-//   rechenergebnis = wertA + wertB;
-// }
+var rechenart = false;
 
 function pressTaste(x) {
-  //console.log(typeof wertA, wertA, typeof wertB, wertB);
-  if (wertB == false) {
+  if (wertB === false) {
     addNumberA(x);
   } else {
     addNumberB(x);
@@ -17,38 +12,128 @@ function pressTaste(x) {
 }
 
 function addNumberA(x) {
-  if (wertA == false) {
+  if (wertA === false) {
     wertA = x;
     document.getElementById("ergebnis").innerHTML = wertA;
     console.log("wertA = " + wertA);
   } else {
     wertA = wertA + String(x);
-    document.getElementById("ergebnis").innerHTML = wertA;
     console.log("wertA = " + wertA);
+    document.getElementById("ergebnis").innerHTML = wertA;
   }
 }
 
 function addNumberB(x) {
-  if (wertB !== false) {
+  if (wertB === true) {
     wertB = x;
-    console.log("wertB = " + wertB);
+    console.log("if-wertB = " + wertB);
     document.getElementById("ergebnis").innerHTML = wertB;
   } else {
     wertB = wertB + String(x);
-    console.log("wertB = " + wertB);
+    console.log("else-wertB = " + wertB);
     document.getElementById("ergebnis").innerHTML = wertB;
   }
 }
 
-function berechnung(y) {
+function berechnungPlus() {
+  if (wertB === true) {
+    return;
+  } else if (wertA !== false && wertB !== false) {
+    rechenergebnis = Number(wertA) + Number(wertB);
+    document.getElementById("ergebnis").innerHTML = rechenergebnis;
+    wertA = rechenergebnis;
+    wertB = true;
+    rechenart = "plus";
+    console.log(rechenergebnis);
+  } else {
+    wertB = true;
+    rechenart = "plus";
+    console.log("WertA gespeichert:", wertA);
+  }
+}
+
+function berechnungMinus() {
+  if (wertB === true) {
+    return;
+  } else if (wertA !== false && wertB !== false) {
+    rechenergebnis = Number(wertA) - Number(wertB);
+    document.getElementById("ergebnis").innerHTML = rechenergebnis;
+    wertA = rechenergebnis;
+    wertB = true;
+    rechenart = "minus";
+    console.log(rechenergebnis);
+  } else {
+    wertB = true;
+    rechenart = "minus";
+    console.log("WertA gespeichert:", wertA);
+  }
+}
+
+function berechnungMal() {
   if (wertA !== false && wertB !== false) {
+    rechenergebnis = Number(wertA) * Number(wertB);
+    document.getElementById("ergebnis").innerHTML = rechenergebnis;
+    wertA = rechenergebnis;
+    wertB = true;
+    rechenart = "mal";
+    console.log(rechenergebnis);
+  } else {
+    wertB = true;
+    rechenart = "mal";
+    console.log("WertA gespeichert:", wertA);
+  }
+}
+
+function berechnungDurch() {
+  if (wertA !== false && wertB !== false) {
+    rechenergebnis = Number(wertA) / Number(wertB);
+    document.getElementById("ergebnis").innerHTML = rechenergebnis;
+    wertA = rechenergebnis;
+    wertB = true;
+    rechenart = "durch";
+    console.log(rechenergebnis);
+  } else {
+    wertB = true;
+    rechenart = "durch";
+    console.log("WertA gespeichert:", wertA);
+  }
+}
+
+function istgleich() {
+  if (wertB === true) {
+    return;
+  }
+  if (rechenart === "plus" && wertB !== true) {
     rechenergebnis = Number(wertA) + Number(wertB);
     document.getElementById("ergebnis").innerHTML = rechenergebnis;
     console.log(rechenergebnis);
     wertA = rechenergebnis;
     wertB = true;
-  } else {
-    wertB = true;
-    console.log("WertA gespeichert:", wertA);
   }
+  if (rechenart === "minus" && wertB !== true) {
+    rechenergebnis = Number(wertA) - Number(wertB);
+    document.getElementById("ergebnis").innerHTML = rechenergebnis;
+    console.log(rechenergebnis);
+    wertA = rechenergebnis;
+    wertB = true;
+  }
+  if (rechenart === "mal") {
+    rechenergebnis = Number(wertA) * Number(wertB);
+    document.getElementById("ergebnis").innerHTML = rechenergebnis;
+    wertA = rechenergebnis;
+    wertB = true;
+  }
+  if (rechenart === "durch") {
+    rechenergebnis = Number(wertA) / Number(wertB);
+    document.getElementById("ergebnis").innerHTML = rechenergebnis;
+    wertA = rechenergebnis;
+    wertB = true;
+  }
+}
+
+function reset() {
+  wertA = false;
+  wertB = false;
+  rechenergebnis = 0;
+  document.getElementById("ergebnis").innerHTML = rechenergebnis;
 }
